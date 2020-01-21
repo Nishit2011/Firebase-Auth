fc77da53767fe2aab9eb36d46c644a8481065bee
Nishit Ranjan <nishranjan@deloitte.com>
2020-01-21 07:49:00 +0530

    checking auth status, if user is logged in or logged out

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,3 +1,14 @@
+//listen for auth status changes
+//everytime a user logs in or logs out, this method is fired
+auth.onAuthStateChanged(user => {
+  console.log(user);
+  if (user) {
+    console.log("user logged in:", user);
+  } else {
+    console.log("user logged out");
+  }
+});
+
 //signup
 const signupForm = document.querySelector("#signup-form");
 signupForm.addEventListener("submit", e => {
@@ -23,9 +34,7 @@ signupForm.addEventListener("submit", e => {
 const logout = document.querySelector("#logout");
 logout.addEventListener("click", e => {
   e.preventDefault();
-  auth.signOut().then(() => {
-    console.log("user signed out");
-  });
+  auth.signOut();
 });
 
 //login
@@ -38,8 +47,6 @@ loginForm.addEventListener("submit", e => {
   const password = loginForm["login-password"].value;
 
   auth.signInWithEmailAndPassword(email, password).then(cred => {
-    console.log(cred.user);
-
     //close the login modal and reset the form
     const modal = document.querySelector("#modal-login");
     M.Modal.getInstance(modal).close();
